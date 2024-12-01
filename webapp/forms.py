a0801d3524d50a2f3486ccd2e1f9b17ee6d772b1
Saranya2004 <116064710+Saranya2004@users.noreply.github.com>
2024-12-01 12:49:13 +0530

    Code commit

---
+++ webapp/forms.py
@@ -0,0 +1,40 @@
+#default
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import Record
+
+from django import forms
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
+# - Register/Create a User
+
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+# - Login a user    
+    
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=TextInput())
+    password =  forms.CharField(widget=PasswordInput())
+
+
+# - Create a record form
+class CreateRecordForm(forms.ModelForm):
+
+    class Meta:
+        model = Record
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
+
+    
+# - Update a record form
+class UpdateRecordForm(forms.ModelForm):
+
+    class Meta:
+        model = Record
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']    
